96cadcc44dbc0aedf0088be7b2435f2345f67843
thesecretofwind <2586404691@qq.com>
2024-04-01 08:14:01 +0800

    feat：路由页面

---
+++ packages/blog-fontend/src/pages/Home.vue
@@ -0,0 +1,26 @@
+<!-- 首页 -->
+<template>
+  <div>
+      <blog-header></blog-header>
+      <div class="container">
+          <el-row  :gutter="30">
+              <el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+                  <articlelist></articlelist>
+              </el-col>
+              <el-col :sm="24"  :md="8" >
+                  <rightlist></rightlist>
+              </el-col>
+          </el-row>
+      </div>
+      hello world
+  </div>
+</template>
+
+<script setup lang="ts">
+import BlogHeader from '@/components/header.vue'
+import articlelist from '@/components/articlelist.vue'
+import rightlist from '@/components/rightlist.vue'
+</script>
+
+<style lang="less" scoped>
+</style>
